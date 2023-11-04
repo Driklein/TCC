8f52dd537ad734560939cc94c6d5573e9f236511
Rodrigo Siveris Klein <rodrigosiverisklein@gmail.com>
2023-11-04 00:24:57 -0300

    arrumando arquivos

--- detect_vehicles.py
+++ detect_vehicles.py
@@ -4,10 +4,10 @@ import csv
 model = YOLO('yolov8m.pt')
 
 results = model.track(
-	source='C:/Users/Rodrigo/Desktop/TCC/DataBase2/Insight-MVT_Annotation_Test/MVI_39311', 
+	source='C:/Users/rodri/Desktop/TCC/archive', 
 	show=True, 
 	save=True,
-	save_dir='C:/Users/Rodrigo/Desktop/TCC/runs', 
+	save_dir='C:/Users/rodri/Desktop/TCC/runs', 
 	tracker='bytetrack.yaml',
 	classes=(2,3,5,7)
 	#0: person
